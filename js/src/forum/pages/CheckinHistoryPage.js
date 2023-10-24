9c0d2a60ab04eb4eb180b4ee672622534a7baa68
liufei <liufei@liufei.info>
2023-10-24 18:25:19 +0800

    大于今天的日期点击不弹出补签窗口

--- js/src/forum/pages/CheckinHistoryPage.js
+++ js/src/forum/pages/CheckinHistoryPage.js
@@ -81,6 +81,10 @@ export default class CheckinHistoryPage extends UserPage {
   async openCreateModal(info) {
     const refetchEvents = this.calendar.refetchEvents.bind(this.calendar);
 
+    if (new Date(info.dateStr).getTime() > new Date()) {
+      return false;
+    }
+
     const list = this.history.payload.data;
     for (var index in list) {
       if (list[index].attributes.start == info.dateStr) {
